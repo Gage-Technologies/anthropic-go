f7dab72fb23951abf948d5080af0dbd4095ff9f1
Sam Ulrich <40002776+sam-ulrich1@users.noreply.github.com>
2024-03-14 09:02:12 -0500

    Added Haiku model

--- models.go
+++ models.go
@@ -3,8 +3,10 @@ package anthropic
 const (
 	ModelClaude3Opus           = "claude-3-opus-20240229"
 	ModelClaude3Sonnet         = "claude-3-sonnet-20240229"
+	ModelClaude3Haiku          = "claude-3-haiku-20240307"
 	ModelClaude3Opus20240229   = "claude-3-opus-20240229"
 	ModelClaude3Sonnet20240229 = "claude-3-sonnet-20240229"
+	ModelClaude3Haiku20240307  = "claude-3-haiku-20240307"
 )
 
 const (
