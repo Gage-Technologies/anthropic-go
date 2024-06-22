6e33f33f8f9de18a4bc5d9bdc4eb6980fb92a680
Sam Ulrich <40002776+sam-ulrich1@users.noreply.github.com>
2024-06-21 20:58:35 -0500

    Add Sonnet 3.5

--- models.go
+++ models.go
@@ -1,6 +1,8 @@
 package anthropic
 
 const (
+	ModelClaude35Sonnect = "claude-3-5-sonnet-20240620"
+
 	ModelClaude3Opus           = "claude-3-opus-20240229"
 	ModelClaude3Sonnet         = "claude-3-sonnet-20240229"
 	ModelClaude3Haiku          = "claude-3-haiku-20240307"
